d166d9c5c11be5cba616ba15d83499677ef9ac67
Alex Desjardins <ardesjardins91@gmail.com>
2020-12-31 13:10:13 -0500

    Create like type

--- app/graphql/types/like_type.rb
+++ app/graphql/types/like_type.rb
@@ -1,10 +1,10 @@
 module Types
   class LikeType < Types::BaseObject
     field :id, ID, null: false
-    field :user_id, Integer, null: true
-    field :post_id, Integer, null: true
-    field :latitude, Float, null: true
-    field :longitude, Float, null: true
+    field :user_id, Integer, null: false
+    field :post_id, Integer, null: false
+    field :latitude, Float, null: false
+    field :longitude, Float, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
