cebd21b714002366dde9ecfee222a039d89f619d
Alex Desjardins <ardesjardins91@gmail.com>
2020-12-31 12:56:50 -0500

    Add destroy_all method to seeds. Fix bug in like seeding.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
-like_user = User.create!(username: Faker::Internet.unique.email, password: '12345', password_confirmation: '12345')
+Like.destroy_all
+Post.destroy_all
+User.destroy_all
+
+like_user1 = User.create!(username: Faker::Internet.unique.email, password: '12345', password_confirmation: '12345')
+like_user2 = User.create!(username: Faker::Internet.unique.email, password: '12345', password_confirmation: '12345')
 
 3.times do
   User.create!(username: Faker::Internet.unique.email, password: '12345', password_confirmation: '12345')
@@ -9,7 +14,6 @@ User.all.each do |user|
 end
 
 Post.all.each do |post|
-  2.times do
-    post.likes.create!(user_id: like_user.id, latitude: Faker::Address.latitude, longitude: Faker::Address.longitude)
-  end
+  post.likes.create!(user_id: like_user1.id, latitude: Faker::Address.latitude, longitude: Faker::Address.longitude)
+  post.likes.create!(user_id: like_user2.id, latitude: Faker::Address.latitude, longitude: Faker::Address.longitude)
 end
