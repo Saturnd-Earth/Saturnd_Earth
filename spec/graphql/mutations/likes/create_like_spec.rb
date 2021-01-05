54369e157d6036c2a3717a9df27892a3bc71ed9c
Alex Desjardins <ardesjardins91@gmail.com>
2021-01-04 19:29:57 -0700

    Add test for CreateLikePayload

--- spec/graphql/mutations/likes/create_like_spec.rb
+++ spec/graphql/mutations/likes/create_like_spec.rb
@@ -10,10 +10,30 @@ module Mutations
           post = create(:post, user: post_user)
           lat = 38.8409
           lon = -105.0423
-          
+
           expect do
-            post '/graphql', params: { query: create_like(user_id: like_user.id, post_id: post.id, latitude: 80.765849, longitude: -92.87357) }
+            post '/graphql', params: { query: create_like(user_id: like_user.id, post_id: post.id, latitude: lat, longitude: lon) }
           end.to change { Like.count }.by(1)
+        end
+
+        it 'returns "like" data' do
+          like_user = create(:user, username: "user1@turing.com", password: "password")
+          post_user = create(:user, username: "user2@turing.com", password: "password")
+          post = create(:post, user: post_user)
+          lat = 38.8409
+          lon = -105.0423
+
+          post '/graphql', params: { query: create_like(user_id: like_user.id, post_id: post.id, latitude: lat, longitude: lon) }
+          json = JSON.parse(response.body)
+          data = json['data']['createLike']['like']
+
+          expect(data).to include(
+            'id'        => be_present,
+            'userId'    => like_user.id,
+            'postId'    => post.id,
+            'latitude'  => lat,
+            'longitude' => lon
+          )
         end
       end
 
@@ -23,10 +43,15 @@ module Mutations
             createLike(input:{
               userId: #{user_id}
               postId: #{post_id}
-              latitude: 80.765849
-              longitude: -92.87357}
-            ) {
-              clientMutationId
+              latitude: #{latitude}
+              longitude: #{longitude}})
+            { like{
+              id
+              userId
+              postId
+              latitude
+              longitude
+              }
             }
           }
         GQL
