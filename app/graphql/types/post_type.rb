1c08994dd623dedf5da77e15cf0e04495468e111
Drew Williams <drewwilliams5280@gmail.com>
2021-01-04 18:27:41 -0700

    Adds likers to posts. Changed liking_users attribute on posts to likers

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -5,6 +5,7 @@ module Types
     field :content, String, null: false
     field :latitude, Float, null: false
     field :longitude, Float, null: false
+    field :likers, [UserType], null: false
     field :ring_min_max, String, null: false
     field :created_at, String, null: false
   end
