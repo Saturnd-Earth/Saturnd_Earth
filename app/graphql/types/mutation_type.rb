4d1945ecc3560dbdaab3f08216958d9a71867057
Drew Williams <drewwilliams5280@gmail.com>
2021-01-07 08:37:10 -0700

    Adds mutation for editing post

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,6 +4,7 @@ module Types
     field :create_like, mutation: Mutations::CreateLike
     field :destroy_like, mutation: Mutations::DestroyLike
     field :create_post, mutation: Mutations::CreatePost
+    field :edit_post, mutation: Mutations::EditPost
     field :destroy_post, mutation: Mutations::DestroyPost
     field :destroy_user, mutation: Mutations::DestroyUser
     field :edit_user, mutation: Mutations::EditUser
---
+++ spec/graphql/mutations/posts/edit_post_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+module Mutations
+  module Posts
+    RSpec.describe EditPost, type: :request do
+      describe '.resolve' do
+        it 'edits a post' do
+          new_user = create(:user, id: 40)
+          new_post = create(:post, user_id: 40, content: "He's left and right handed")
+          require 'pry'; binding.pry
+          expect(Post.first.content).to eq("He's left and right handed")
+          post '/graphql', params: { query: query(post_id: new_post.id) }
+          json = JSON.parse(response.body)
+          data = json['data']['editPost']['post']
+          require 'pry'; binding.pry
+          expect(Post.first.content).to eq("Jk, he's only right-handed")
+        end
+
+      end
+
+      def query(post_id:)
+        <<~GQL
+        mutation{
+          editPost(input:{
+            id: #{post_id},
+            content: "Jk, he's only right-handed",
+            latitude: 71.835532572575,
+            longitude: -118.50910685764141,
+            userId: 40
+          })
+          {
+            post {
+              id
+              content
+            }
+          }
+        }
+        GQL
+      end
+    end
+  end
+end
