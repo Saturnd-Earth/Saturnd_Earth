f254e48717573cb05bc186d5653b8b5412170ea2
Alex Desjardins <ardesjardins91@gmail.com>
2021-01-11 12:15:57 -0700

    Change column name in post table from "content" to "text".

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -2,15 +2,14 @@ module Types
   class PostType < Types::BaseObject
     field :id, ID, null: false
     field :user_id, Integer, null: false
-    field :content, String, null: false
     field :latitude, Float, null: false
     field :longitude, Float, null: false
     field :likers, [UserType], null: false
     field :likes, [LikeType], null: false
     field :ring_min_max, String, null: false
     field :created_at, String, null: false
-    field :url, String, null: false
+    field :text, String, null: true
+    field :url, String, null: true
     field :post_type, String, null: false
-    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_213732) do
+ActiveRecord::Schema.define(version: 2021_01_11_184600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2021_01_09_213732) do
 
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id"
-    t.text "content"
+    t.text "text"
     t.float "latitude"
     t.float "longitude"
     t.integer "ring_min_max", default: 0
