d9a8bf225c4a747255ab587cea2f0dba7b1d9a2d
Drew Williams <drewwilliams5280@gmail.com>
2021-01-12 13:37:48 -0700

    Adds user(id:) query test

--- spec/graphql/queries/users/users_spec.rb
+++ spec/graphql/queries/users/users_spec.rb
@@ -17,25 +17,16 @@ module Queries
           expect(data.last['id']).to eq(user3.id.to_s)
         end
 
-        # it 'can get a post' do
-        #   user1 = create(:user)
-        #   user2 = create(:user)
-        #   user3 = create(:user)
-        #   post1 = create(:post, user_id: user1.id)
-        #   post2 = create(:post, user_id: user2.id)
-        #   post3 = create(:post, user_id: user3.id)
-        #   post4 = create(:post, user_id: user1.id)
-        #   post5 = create(:post, user_id: user2.id)
-        #   post6 = create(:post, user_id: user3.id)
-        #   post7 = create(:post, user_id: user1.id)
-        #   post8 = create(:post, user_id: user2.id)
-        #   post9 = create(:post, user_id: user3.id)
-        #   post '/graphql', params: { query: post_by_id(post_id: post1.id) }
-        #   json = JSON.parse(response.body)
-        #   data = json['data']['post']
-        #   expect(data['id']).to eq(post1.id.to_s)
-        #   expect(data['text']).to eq(post1.text)
-        # end
+        it 'can get a post' do
+          user1 = create(:user)
+          user2 = create(:user)
+          user3 = create(:user)
+          post '/graphql', params: { query: user_by_id(user_id: user1.id) }
+          json = JSON.parse(response.body)
+          data = json['data']['user']
+          expect(data['id']).to eq(user1.id.to_s)
+          expect(data['username']).to eq(user1.username)
+        end
 
       def all_users
         <<~GQL
@@ -48,29 +39,12 @@ module Queries
         GQL
       end
 
-      def post_by_id(post_id:)
+      def user_by_id(user_id:)
         <<~GQL
           {
-            post(id: #{post_id}) {
+            user(id: #{user_id}) {
               id
-              latitude
-              longitude
-              ringMinMax
-              createdAt
-              userId
-              text
-              url
-              likers{
-                id
-                username
-               }
-               likes{
-                 id
-                 userId
-                 latitude
-                 longitude
-                 createdAt
-               }
+              username
             }
           }
         GQL
