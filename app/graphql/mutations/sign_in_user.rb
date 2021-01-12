efe72e871da8b70cdd9db9258d669611c6940965
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2021-01-12 14:26:20 -0700

    Add field for errors in [string] format, adjust logic for testing

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -5,16 +5,31 @@ module Mutations
     argument :credentials, Types::AuthProviderCredentialsInput, required: false
 
     field :user, Types::UserType, null: true
-
+    field :errors, [String], null: false
+    
     def resolve(credentials:)
-      return unless credentials
+      # return unless credentials
 
       user = User.find_by username: credentials[:username]
-
-      return unless user
-      return unless user.authenticate(credentials[:password])
-
-      { user: user}
+      binding.pry
+      if user.authenticate(credentials[:password]) == true
+        {
+          user: user,
+          errors: []
+        }
+      elsif user.authenticate(credentials[:password]) == false
+        {
+          user: nil,
+          errors: "user.errors.full_messages"
+        }
+      end
     end
   end
 end
+
+
+
+# binding.pry
+# if !user.nil?
+#   return unless user
+#   return unless user.authenticate(credentials[:password])
