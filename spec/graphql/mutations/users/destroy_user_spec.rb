24f6cb6f494a7cbc67ca7fbae87aa2eda264e34a
Alex Desjardins <ardesjardins91@gmail.com>
2021-01-12 12:08:14 -0700

    Refactor user destroy spec
    Co-Authored-By: Garrett Gerdsen <garrett.gerdsen@gmail.com>

--- spec/graphql/mutations/users/destroy_user_spec.rb
+++ spec/graphql/mutations/users/destroy_user_spec.rb
@@ -6,35 +6,13 @@ module Mutations
       describe '.resolve' do
         it 'deletes a user' do
           user = create(:user, password: "password")
-          # Creates a user here
-          expect do
-            post '/graphql', params: { query: createUser(username: "New User") }
-          end.to change { User.count }.by(1)
-          expect(User.count).to eq(2)
-          
-          #Delete created user here
-          post '/graphql', params: { query: destroyUser(id: user.id) }
+
           expect(User.count).to eq(1)
+          post '/graphql', params: { query: destroyUser(id: user.id) }
+          expect(User.count).to eq(0)
         end
       end
-    
-      def createUser(username:)
-        <<~GQL
-        mutation {
-          createUser(input:{
-            username: "me@gmail.com"
-            password: "password123"
-          })
-          {
-            user{
-              id
-              username
-            }
-          }
-        }
-        GQL
-      end
-      
+
       def destroyUser(id:)
         <<~GQL
         mutation {
