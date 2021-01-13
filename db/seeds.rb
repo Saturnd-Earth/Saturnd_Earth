522b95d95d8bc0b3cd5e16205914509af01b6e13
Drew Williams <drewwilliams5280@gmail.com>
2021-01-13 12:40:02 -0700

    Edits seeds to avoid ocean areas

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,31 +10,61 @@ like_user2 = User.create!(username: Faker::Internet.unique.email, password: '123
   puts "#{num} users created!"
 end
 
-200.times do |num|
+150.times do |num|
   user = User.order('RANDOM()').first
   user.posts.create!(text: Faker::Quote.most_interesting_man_in_the_world, latitude: rand(39.500000..40.050000), longitude: rand(-105.300000..-104.700000), post_type: "Comment")
   puts "#{num} Denver posts created!"
 end
 
-200.times do |num|
+150.times do |num|
   user = User.order('RANDOM()').first
   user.posts.create!(text: Faker::Quote.most_interesting_man_in_the_world, latitude: rand(37.000001..40.999999), longitude: rand(-109.000000..-102.000000), post_type: "Comment")
   puts "#{num} Colorado posts created!"
 end
 
-200.times do |num|
+150.times do |num|
   user = User.order('RANDOM()').first
   user.posts.create!(text: Faker::Quote.most_interesting_man_in_the_world, latitude: rand(27.000001..47.999999), longitude: rand(-125.000000..-70.000000), post_type: "Comment")
   puts "#{num} United States posts created!"
 end
 
-250.times do |num|
+30.times do |num|
   user = User.order('RANDOM()').first
-  user.posts.create!(text: Faker::Quote.most_interesting_man_in_the_world, latitude: rand(-50.000000..70.000000), longitude: rand(-178.000000..178.000000), post_type: "Comment")
-  puts "#{num} random posts created!"
+  user.posts.create!(text: Faker::Quote.most_interesting_man_in_the_world, latitude: rand(-25.000000..10.000000), longitude: rand(-77.000000..-43.000000), post_type: "Comment")
+  puts "#{num} South American posts created!"
 end
 
-2800.times do |num|
+15.times do |num|
+  user = User.order('RANDOM()').first
+  user.posts.create!(text: Faker::Quote.most_interesting_man_in_the_world, latitude: rand(-33.000000..5.000000), longitude: rand(13.000000..39.000000), post_type: "Comment")
+  puts "#{num} African posts created!"
+end
+
+60.times do |num|
+  user = User.order('RANDOM()').first
+  user.posts.create!(text: Faker::Quote.most_interesting_man_in_the_world, latitude: rand(7.000000..33.000000), longitude: rand(-7.000000..120.000000), post_type: "Comment")
+  puts "#{num} Northern Africa, Middle East, and Southern Asia posts created!"
+end
+
+100.times do |num|
+  user = User.order('RANDOM()').first
+  user.posts.create!(text: Faker::Quote.most_interesting_man_in_the_world, latitude: rand(36.000000..61.000000), longitude: rand(-8.000000..41.000000), post_type: "Comment")
+  puts "#{num} European posts created!"
+end
+
+50.times do |num|
+  user = User.order('RANDOM()').first
+  user.posts.create!(text: Faker::Quote.most_interesting_man_in_the_world, latitude: rand(36.000000..61.000000), longitude: rand(41.000000..120.000000), post_type: "Comment")
+  puts "#{num} Northern Asia and Russian posts created!"
+end
+
+30.times do |num|
+  user = User.order('RANDOM()').first
+  user.posts.create!(text: Faker::Quote.most_interesting_man_in_the_world, latitude: rand(-34.000000..-20.000000), longitude: rand(115.000000..150.000000), post_type: "Comment")
+  puts "#{num} Australian posts created!"
+end
+
+3000.times do |num|
   post = Post.order('RANDOM()').first
   post.increase_ring
   puts "#{num} increased rings!"
