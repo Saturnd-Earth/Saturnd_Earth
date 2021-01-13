bb4315de5688acfcc1d122f4e3f84a86c892842e
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2021-01-13 09:24:53 -0700

    Add testing to error check incorrect password datatype

--- spec/graphql/mutations/users/edit_user_spec.rb
+++ spec/graphql/mutations/users/edit_user_spec.rb
@@ -18,6 +18,22 @@ module Mutations
           expect(user.username).to eq("testname")
 
         end
+        
+        it 'returns an error message when edit is invalid' do
+          User.create!(username: "Original", password: "password")
+          
+          user = User.last
+          
+          expect(user.username).to eq("Original")
+          
+          post '/graphql', params: { query: badEditUser(id: user.id) }
+          
+          json = JSON.parse(response.body)
+          data = json['errors'].first['message']
+          
+          expect(data).to eq("Argument 'password' on InputObject 'EditUserInput' has an invalid value. Expected type 'String'.")
+
+        end
       end
     
       def editUser(id:)
@@ -27,6 +43,24 @@ module Mutations
             id: #{id}
             username: "testname"
             password: "password"
+          })
+          {
+            user{
+              id
+              username
+            }
+          }
+        }
+        GQL
+      end
+      
+      def badEditUser(id:)
+        <<~GQL
+        mutation {
+          editUser(input:{
+            id: #{id}
+            username: "testname"
+            password: 1
           })
           {
             user{
